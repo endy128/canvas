fcc88835eabe3fcfe7a417a1d16c4d0598442241
Neil Douglas <endy64@icloud.com>
2022-12-17 15:43:42 +0000

    Added mouse interaction

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ canvas.height = window.innerHeight
 canvas.width = window.innerWidth
 
 let c = canvas.getContext('2d')
+let speed = 10
+let distance = 50
+let size = 50
+let totalBalls = 100
+let deceleration = 0.02
+
+let mouse = {
+  x: null,
+  y: null,
+}
+
+window.addEventListener('mousemove', function (e) {
+  mouse.x = e.x
+  mouse.y = e.y
+})
 
 const circleFactory = (x, y, dx, dy, radius, hue) => {
   let self = {
@@ -33,13 +48,53 @@ const circleFactory = (x, y, dx, dy, radius, hue) => {
 
       self.x += self.dx
       self.y += self.dy
+
+      // mouse interactivity
+      if (
+        mouse.x - self.x < distance &&
+        mouse.x - self.x > -distance &&
+        mouse.y - self.y < distance &&
+        mouse.y - self.y > -distance
+      ) {
+        let xchange = Math.floor(self.x - mouse.x) / speed
+        let ychange = Math.floor(self.y - mouse.y) / speed
+        if (xchange > 0) {
+          self.dx = 0.5 - Math.floor(mouse.x - self.x) / speed
+        }
+        if (xchange < 0) {
+          self.dx = -0.5 - Math.floor(mouse.x - self.x) / speed
+        }
+
+        if (ychange > 0) {
+          self.dy = 0.5 - Math.floor(mouse.y - self.y) / speed
+        }
+        if (ychange < 0) {
+          self.dy = -0.5 - Math.floor(mouse.y - self.y) / speed
+        }
+
+        // self.dx = Math.floor(self.x - mouse.x) / speed
+        // self.dy = Math.floor(self.y - mouse.y) / speed
+      } else {
+        if (self.dx > 0.4) {
+          self.dx -= deceleration
+        }
+        if (self.dy < -0.4) {
+          self.dy += deceleration
+        }
+        if (self.dy > 0.4) {
+          self.dy -= deceleration
+        }
+        if (self.dy < -0.4) {
+          self.dy += deceleration
+        }
+      }
     },
   }
 }
 
 let circleArray = []
-for (let i = 0; i < 200; i++) {
-  let radius = Math.floor(Math.random() * 10 + 1)
+for (let i = 0; i < totalBalls; i++) {
+  let radius = Math.floor(Math.random() * size + 1)
   let hue = Math.floor(Math.random() * 360)
   let x = Math.random() * (window.innerWidth - radius * 2) + radius
   let y = Math.random() * (window.innerHeight - radius * 2) + radius
